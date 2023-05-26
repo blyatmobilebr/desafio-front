2005bfbda4f1918d216051bba70499e3588dcd3b
blyatmobilebr <lucaslima2458@gmail.com>
2023-05-26 18:16:11 -0300

    [Refactor]: Move SignUp files to src/pages
    This commit refactors the project structure by moving the SignUp files to the src/pages directory.

---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import "./SignUp.css"
+import { Link, useNavigate } from "react-router-dom";
+
+function SignUp() {
+    const [form, setForm] = useState({
+        nome: "",
+        nome_loja: "",
+        email: "",
+        senha: "",
+        senhaConfirmacao: ""
+    });
+
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+    const navigate = useNavigate();
+
+
+    function handleInputChange(e) {
+        const { name, value } = e.target;
+
+        if (name) {
+            setForm({
+                ...form,
+                [name]: value
+            });
+        }
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+    
+        if (!form.nome || !form.email || !form.nome_loja || !form.senha || !form.senhaConfirmacao) {
+            setError("TODOS os campos são obrigatórios!");
+            setSuccess("");
+            
+        } else if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(form.email))) {
+            setError("E-mail está num formato inválido!");
+            setSuccess("");
+        } else if (form.senhaConfirmacao !== form.senha) {
+            setError("As senhas não coincidem!");
+            setSuccess("");
+        } else {
+            setError("");
+            navigate("/sign-in")
+        }
+
+        localStorage.setItem("nome", JSON.stringify(form.nome));
+        localStorage.setItem("nome_loja", JSON.stringify(form.nome_loja));
+        localStorage.setItem("email", JSON.stringify(form.email));
+        localStorage.setItem("senha", JSON.stringify(form.senha));
+        localStorage.setItem("senhaConfirmacao", JSON.stringify(form.senhaConfirmacao));
+    }
+        
+
+    return (
+        <div className="wrapper">
+            <form className="form" onSubmit={handleSubmit}>
+                <h4 className="form-title">Criar uma conta</h4>
+
+                <div className="label-div">
+                    <label className="label">Seu nome</label>
+                </div>
+                <input
+                    className="input"
+                    type="text"
+                    onChange={(event) => handleInputChange(event)}
+                    value={form.nome}
+                    name="nome"
+                />
+
+                <label className="label">Nome da loja</label>
+                <input
+                    className="input"
+                    type="text"
+                    onChange={(event) => handleInputChange(event)}
+                    value={form.nome_loja}
+                    name="nome_loja"
+                />
+
+                <label className="label">E-mail</label>
+                <input
+                    className="input"
+                    type="text"
+                    onChange={(event) => handleInputChange(event)}
+                    value={form.email}
+                    name="email"
+                />
+
+                <label className="label">Senha</label>
+                <input
+                    className="input"
+                    type="password"
+                    onChange={(event) => handleInputChange(event)}
+                    value={form.senha}
+                    name="senha"
+                />
+
+                <label className="label">Repita a senha</label>
+                <input
+                    className="input"
+                    type="password"
+                    onChange={(event) => handleInputChange(event)}
+                    value={form.senhaConfirmacao}
+                    name="senhaConfirmacao"
+                />
+
+                {error && <span className="error">{error}</span>}
+
+                <button className="form-button send-button" type="submit">
+                    CRIAR CONTA
+                </button>
+
+                <p className="paragraph">
+                    Já possui uma conta? <Link className="link" to="/sign-in" >ACESSE</Link>
+                </p>
+            </form>
+        </div>
+    );
+}
+
+export default SignUp;
