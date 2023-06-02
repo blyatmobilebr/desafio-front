d053f33c4d6b71b25c9481edf3932388365a1e58
blyatmobilebr <lucaslima2458@gmail.com>
2023-06-02 17:39:28 -0300

    feat: Configure routes in App component
    This commit adds the necessary routes and components in the App.jsx file to handle navigation in the application. The routes include Login, SignUp, Produtos, AdicionarProdutos, EditarProduto, EditarPerfil, and ErrorPage. This configuration allows users to navigate between different pages and access the corresponding components based on the route path.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
-import Login from './pages/Login/Login';
+import EditarPerfil from './pages/EditarPerfil/EditarPerfil';
+import Perfil from './pages/Perfil/Perfil';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
 import Produtos from './pages/Produtos/Produtos';
 import SignUp from './pages/SignUp/SignUp';
-import {Routes, Route} from "react-router-dom"
-
+import Login from './pages/Login/Login';
+import AdicionarProdutos from './pages/AdicionarProdutos/AdicionarProdutos';
+import EditarProduto from './pages/EditarProduto/EditarProduto';
 
 function App() {
   return (
+    <>
     <Routes>
       <Route path="/" element={<Login />}>
         <Route path="/sign-in" element={<Login />} />
       </Route>
-
-      <Route path="/sign-up" element={<SignUp />} />
+      
+      <Route path="/usuarios" element={<SignUp />} />
       <Route path="/produtos" element={<Produtos />} />
+      <Route path="/produtos/novo" element={<AdicionarProdutos />} />
+      <Route path="/produtos/editar" element={<EditarProduto />} /> 
+      <Route path="/perfil/editar" element={<EditarPerfil />} />
 
-
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
+  </>
   )
 }
 
